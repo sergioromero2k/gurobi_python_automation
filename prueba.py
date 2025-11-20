8639ddf39441ab4c7c9848b236d8526a3d6092a6
Sergio Alejandro <sergioromero2k@gmail.com>
2025-11-20 17:01:00 +0100

    Algunas avances neovim + visual

---
+++ prueba.py
@@ -0,0 +1,27 @@
+# Recorriendo una tupla en un diccionario
+costos = {("T1", "Tarea1"): 9, ("T2", "Tarea2"): 7}
+print(costos["T1", "Tarea1"])
+
+Trabajador = "T2"
+Tarea = "Tarea2"
+print(costos[Trabajador, Tarea])
+
+for (i, j), valor in costos.items():
+    print(i, j, valor)
+
+Trabajadores = ["T1", "T2"]
+Tareas = ["Tarea1", "Tarea2"]
+
+# for i in Trabajadores:
+#    for j in Tareas:
+#        print(i, j, costos[i, j])
+
+t1 = ("T1", "Tarea1")
+print(t1)
+print(t1[0])
+print(t1[1])
+
+# Tupla de 3 elementos
+t2 = ("T1", "Tarea1", 10)
+print(t2[2])
+print(t2[1])
